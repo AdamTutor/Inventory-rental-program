65501b1abc2f40b2d87c5e0bc2f82c423ff3ad33
AdamTutor <atutor@basecampcodingacademy.org>
2017-01-27 13:57:01 -0600

    removed global instance variable from test file, added instance in each test.

--- test_rental_classes.py
+++ test_rental_classes.py
@@ -2,7 +2,6 @@ from Rental_classes import *
 import pytest
 
 
-item = Item('Ps4',400,40,3,20)
 
 
 
@@ -12,20 +11,25 @@ class Test_Item():
 
 
     def test_get_name(self):
+        item = Item('Ps4',400,40,3,20)
         assert item.get_name() == 'Ps4'
     
 
     def test_get_replacement_value(self):
+        item = Item('Ps4',400,40,3,20)
         assert item.get_replacement_value() == 400
 
 
     def test_get_deposit_value(self):
+        item = Item('Ps4',400,40,3,20)
         assert item.get_deposit_value() == 40
 
 
     def test_get_price(self):
+        item = Item('Ps4',400,40,3,20)
         assert item.get_price() == 3
 
 
     def test_get_quantity(self):
+        item = Item('Ps4',400,40,3,20)
         assert item.get_quantity() == 20
