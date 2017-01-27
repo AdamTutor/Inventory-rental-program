a070868b1e539f50b86b959b69bd0c0edcdbd644
AdamTutor <atutor@basecampcodingacademy.org>
2017-01-27 15:53:06 -0600

    added read, view, update functionality to inventory.py(dev phase)

--- inventory.py
+++ inventory.py
@@ -1,20 +1,43 @@
 import csv
+from Rental_classes import *
 
 
 def get_inv(filename):
+    """Takes in a csv file as a parameter and reads 
+    the file and outputs a cvs.reader object that is converted to a list of lists."""
     inventory_list = []
     with open(filename, newline='') as inv:
+    #csv.reader(file to be read, singe character to seperate feilds, A one-character string used to quote fields containing special characters)
         inventory = csv.reader(inv, delimiter=' ', quotechar='|')
-        for row in inventory:
-            item = ', '.join(row)
-            inventory_list.append(item)
-    return inventory_list
+        # returns list of lists from reader object. 
+        return list(inventory)
 
 
 def view_inv(inventory_list):
     for item in inventory_list:
-        item = item.split(",")
-        print(item)
+        print(item[0].split()[0])
 
-view_inv(get_inv('inventory.csv'))
+# view_inv(get_inv("inventory.csv"))
 
+
+def update_inventory():
+    with open("inventory.csv", "a", newline='') as myfile:
+    
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        name = 'ps4'
+        replacement_value = 400
+        deposit_value = 40
+        price = 3
+        quanity = 20
+        wr.writerow([name, replacement_value, deposit_value, price, quanity])
+
+
+# with open('inventory.csv', newline='') as inv:
+#         inventory = csv.reader(inv, delimiter=' ', quotechar='|')
+#         l = list(inventory)
+#         print(l[0][0].split(",")[0])
+   
+
+
+l = get_inv('inventory.csv')
+print(l)
