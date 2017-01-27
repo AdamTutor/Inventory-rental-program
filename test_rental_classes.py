42c4fc9162fea939a66002fcc759bd8552379289
AdamTutor <atutor@basecampcodingacademy.org>
2017-01-27 13:45:38 -0600

    added test cases for attributes on Item class in test_rental_classes.py

---
+++ test_rental_classes.py
@@ -0,0 +1,29 @@
+from Rental_classes import * 
+import pytest
+
+
+item = Item('Ps4',400,40,3,20)
+
+
+
+
+
+class Test_Item():
+    def test_get_name(self):
+        assert item.get_name() == 'Ps4'
+    
+
+    def test_get_replacement_value(self):
+        assert item.get_replacement_value() == 400
+
+
+    def test_get_deposit_value(self):
+        assert item.get_replacement_value() == 40
+
+
+    def test_get_price(self):
+        assert item.get_price() == 3
+
+
+    def test_get_quantity(self):
+        assert item.get_quantity(self) == 20
