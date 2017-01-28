cf9b6df127f2db51ddbd9d491427a753c28d0e34
AdamTutor <atutor@basecampcodingacademy.org>
2017-01-27 21:33:04 -0600

    added transaction.csv file, turned program into a function

--- rental_program.py
+++ rental_program.py
@@ -1,8 +1,15 @@
-print('Are you wanting to do a customer or manager action')
-action = input('c or m\n')
+def program():
+    print('Are you wanting to do a customer or manager action')
+    action = input('c or m\n')
+    if action == "c":
+        print("Are you renting or returning an item? ")
+        a = input('rent or return? \n')
+     
+    elif action == "m":
+     
+        print('m')
+    else:
+        print("invalid input")
+        program()
 
-if action.strip().lower() == 'm':a = []
 
-
-print("Are you returning or renting a item?")
-Action = input("return or rent?\n")
