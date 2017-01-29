67e5c32720974ce28978f1b2bd8f64bdf94fa160
AdamTutor <atutor@basecampcodingacademy.org>
2017-01-29 11:43:14 -0600

    deleted test for methods that were removed from Item class

--- test_rental_classes.py
+++ test_rental_classes.py
@@ -9,32 +9,6 @@ import pytest
  # Testing all attributes for Test class
 class Test_Item():
 
-
-    def test_get_name(self):
-        item = Item('Ps4',400,40,3,20)
-        assert item.get_name() == 'Ps4'
-    
-
-    def test_get_replacement_value(self):
-        item = Item('Ps4',400,40,3,20)
-        assert item.get_replacement_value() == 400
-
-
-    def test_get_deposit_value(self):
-        item = Item('Ps4',400,40,3,20)
-        assert item.get_deposit_value() == 40
-
-
-    def test_get_price(self):
-        item = Item('Ps4',400,40,3,20)
-        assert item.get_price() == 3
-
-
-    def test_get_quantity(self):
-        item = Item('Ps4',400,40,3,20)
-        assert item.get_quantity() == 20
-
-
     def test_item_rented(self):
         item = Item('Ps4',400,40,3,20)
         original_quantity = item.quantity
