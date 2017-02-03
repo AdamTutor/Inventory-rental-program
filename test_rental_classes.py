e5d23ffb1ca6c64d4c5675d6ce6e1c8948e2e555
AdamTutor <atutor@basecampcodingacademy.org>
2017-02-03 14:32:25 -0600

    changed variables in test file, added new test for writer_row()

--- test_rental_classes.py
+++ test_rental_classes.py
@@ -1,27 +1,16 @@
-from Rental_classes import * 
+from Rental_classes import *
 import pytest
 import os
 import datetime
 import csv
 from inventory import *
 
-# Helper functions for mocking up files needed to test
-def write_row(filename, values_to_write):
-    """ (file, list) --> None
-    Takes in a filename and list of data to be written in a row on the file.
-    """
-    with open(filename, 'a', newline='') as file:
-        writer = csv.writer(file)
-        for row in values_to_write:
-            writer.writerow(row)
-    with open(filename) as file:
-        content = file.read()
-        if len(content) > 0:
-            return True
 
 
+
+# Helper functions for testing.
 def create_file(filename):
-    "creates a file called test.csv"   
+    "creates a file called test.csv"
     with open(filename, 'w') as tfile:
         return tfile.close()
 
@@ -30,36 +19,42 @@ def create_file(filename):
  # Testing all attributes for Test class
 class Test_Item():
     def test_class_Item(self):
-        i = Item('ps4', 400, 40, 3,20)
+        i = Item('ps4', 400, 40, 3, 20)
         assert i.name == 'ps4'
         assert i.replacement_value == 400
         assert i.deposit_value == 40
         assert i.price == 3
         assert i.quantity == 20
-        assert str(i) ==  "product: " + 'ps4' + "\nreplacement value: " + '400'+\
+        assert str(i) ==  "product: " + 'ps4' + "\nreplacement value: " + '400' + \
               "\nDeposit value: " + '40'+ "\nprice: " + \
                 '3'+ "\nquantity: " + '20'
 
     def test_in_stock(self):
-         a = Item('ps4', 400, 40, 3,0)
-         i = Item('ps4', 400, 40, 3,20)
-         assert i.in_stock() == True
-         assert a.in_stock() == False
+        a = Item('ps4', 400, 40, 3, 0)
+        i = Item('ps4', 400, 40, 3, 20)
+        assert i.in_stock() == True
+        assert a.in_stock() == False
 
 
 
 class Test_Transaction():
-    def test_class_Transaction(self):
-        T = Transaction(71017, 'ps4', 'pending')
-        assert T.datetime == 71017
-        assert T.item == 'ps4'
-        assert T.status == 'pending'
-        assert str(T) == "date: " + '71017' + " Item " + 'ps4' + " status " + 'pending'
+    def test_class_transaction(self):
+        trans = Transaction(71017, 'ps4', 'pending')
+        assert trans.datetime == 71017
+        assert trans.item == 'ps4'
+        assert trans.status == 'pending'
+        assert str(trans) == "date: " + '71017' + " Item " + 'ps4' + " status " + 'pending'
+
+def test_create_file():
+    'Tests that create_file() function creates a empty file'
+    create_file('test.csv')
+    assert os.path.isfile("/home/basecamp/Projects/Inventory-rental-program/test.csv")
 
 
 def test_get_file_contents():
+    "tests that get_file_contents() reads and returns the content of a file in a list of lists"
     create_file('test.csv')
-    write_row('test.csv', [['TEST','Test', 'test']])
+    write_row('test.csv', [['TEST', 'Test', 'test']])
     Test_inventory = get_file_contents('test.csv')
     assert Test_inventory[0][0] == 'TEST'
     assert Test_inventory[0][1] == 'Test'
@@ -69,7 +64,8 @@ def test_get_file_contents():
 
 def test_get_item_by_name():
     create_file("test.csv")
-    write_row("test.csv", [['ps4', 400, 40, 3,20],['xbox1', 400, 40, 3,20],['TEST', 400, 40, 3,20]])
+    write_row("test.csv", [['ps4', 400, 40, 3, 20], \
+            ['xbox1', 400, 40, 3, 20], ['TEST', 400, 40, 3, 20]])
     test_inventory = get_file_contents('test.csv')
     name = get_item_by_name(test_inventory, 'ps4')
     t = get_item_by_name(test_inventory, 'xbox1')
@@ -147,6 +143,16 @@ def test_view_trans():
 #      total, "sales tax: ", tax, "total: ", final_total
 
 
+
+def test_write_row():
+    create_file('test.csv')
+    write_row('test.csv', [['TEST', 'TEST', 'TEST'], ['TEST', 'TEST', 'TEST']])
+    with open('test.csv') as file:
+        content = file.read()
+    assert content == ('TEST,TEST,TEST\nTEST,TEST,TEST\n')
+    os.remove("test.csv")
+
+    
 
 
 
